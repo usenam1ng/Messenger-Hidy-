33c60b53cac0d78e95f3456f83ca3c7fa5fa7acf
Vladislav Konyukov <63218896+usenam1ng@users.noreply.github.com>
2024-04-25 13:45:51 +0700

    Add files via upload

---
+++ server/init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE autor (
+    usr VARCHAR(255),
+    paswd VARCHAR(255)
+);
+
+CREATE TABLE usr_info (
+    usr VARCHAR(255),
+    openkey TEXT
+);
+
+CREATE TABLE chat (
+    logn VARCHAR(255),
+    frm VARCHAR(255),
+    mess TEXT,
+    idd INTEGER
+);
+
